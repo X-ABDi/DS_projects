cdc7f7ce4edaba873de1d742e068b8b6d397bde2
fateme Abdi <154475529+X-ABDi@users.noreply.github.com>
2024-10-20 21:57:21 +0330

    testcase needed

--- CA1/q1.py
+++ CA1/q1.py
@@ -1,1 +1,27 @@
+import sys
 
+def main ():
+    i = input()
+    i.strip()    
+    output = "" 
+    sign = "" 
+    if i[0] == '+' or i[0] == '-':
+        sign = i[0]
+        i = i[1:]
+       
+    for j in range(0, len(i)):
+        if i[j].isdigit():
+            output += i[j] 
+        else:
+            break       
+    if output == "":
+        print(0)
+    elif int(output) > sys.maxsize and sign == '+':
+        print(sys.maxsize)
+    elif int(output) > sys.maxsize and sign == '-':
+        print(-sys.maxsize - 1)        
+    else:
+        print(sign,int(output), sep="")    
+    
+       
+main ()
